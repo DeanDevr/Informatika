76495e691d5d6038f243ceb35bf1586ec21e97e4
DeanDevr <168057666+DeanDevr@users.noreply.github.com>
2024-05-17 10:45:25 +0700

    dean

--- script.js
+++ script.js
@@ -14,4 +14,3 @@ function appendToDisplay(value) {
                                             document.getElementById("display").value = "Error";
                                                 }
                                                 }
-}
